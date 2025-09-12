7e5a9ed86ff8ab3cfd1027e7da03c1bef4365957
knowzek <71522616+knowzek@users.noreply.github.com>
2025-09-12 09:43:31 -0700

    Update fortellis.py

--- fortellis.py
+++ fortellis.py
@@ -224,7 +224,10 @@ def get_token():
 
 
 def get_recent_leads(token, since_minutes=10):
-    since = (datetime.utcnow() - timedelta(days=6, hours=20)).isoformat()  "Z"
+    # go back ~1 week (6 days + 20 hours) and format as ISO 8601 UTC with "Z"
+    since = (datetime.utcnow() - timedelta(days=6, hours=20)) \
+        .isoformat() + "Z"
+
     url = f"{BASE_URL}/sales/elead/v1/leads/search-delta?since={since}&page=1&pageSize=100"
     headers = {
         "Authorization": f"Bearer {token}",
