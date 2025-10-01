3f0fa3cb42d525c9e8875247e73897f939795c19
knowzek <71522616+knowzek@users.noreply.github.com>
2025-10-01 14:59:59 -0700

    Update rooftops.py

--- rooftops.py
+++ rooftops.py
@@ -6,31 +6,31 @@ SUBSCRIPTION_TO_ROOFTOP = {
     "a4efeb74-2289-43d5-9814-1049fd35e894": {
         "name":    "Mission Viejo Kia",
         "address": "28802 Marguerite Pkwy, Mission Viejo, CA 92692",
-        "sender":  "sales@missionviejokia.com",
+        "sender":  "knowzek@gmail.com",
     },
     # Tustin Mazda
     "7a05ce2c-cf00-4748-b841-45b3442665a7": {
         "name":    "Tustin Mazda",
         "address": "28 Auto Center Dr, Tustin, CA 92782",
-        "sender":  "sales@tustinmazda.com",
+        "sender":  "knowzek@gmail.com",
     },
    # Tustin Kia
     "c27d7f4f-4a4c-45c8-8154-a5de48421fc3": {
         "name":    "Tustin Kia",
         "address": "",
-        "sender":  "sales@tustinkia.com",
+        "sender":  "knowzek@gmail.com",
     },
   # Tustin Hyundai
     "2c61b27b-b239-4b54-bd34-dfd73aa5a568": {
         "name":    "Tustin Hyundai",
         "address": "16 Auto Center Dr, Tustin, CA 92782",
-        "sender":  "sales@tustinhyundai.com",
+        "sender":  "knowzek@gmail.com",
     },
   # Huntington Beach Mazda
     "cbb4a4f18-1693-4450-a08e-40d8df30c139": {
         "name":    "Huntington Beach Mazda",
         "address": "16800 Beach Blvd, Huntington Beach, CA 92647",
-        "sender":  "sales@huntingtonbeachmazda.com",
+        "sender":  "knowzek@gmail.com",
     },
 }
 
